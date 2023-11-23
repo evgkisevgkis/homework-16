8e68fea3ee8cba085aa93d538e380fc7f60407ad
Johnny <evgkisevgkis@gmail.com>
2023-11-23 14:58:28 +0500

    создание модели для пользователей

--- app.py
+++ app.py
@@ -8,6 +8,18 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
+class User(db.Model):
+    """Модель для пользователей"""
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(20))
+    last_name = db.Column(db.String(20))
+    age =  db.Column(db.Integer)
+    email = db.Column(db.String(20))
+    role = db.Column(db.String(20))
+    phone = db.Column(db.String(20))
+
+
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
